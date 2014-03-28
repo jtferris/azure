7cf553ca578d49335766f1b8f4faec9aa269fe79
jtferris <j.t.ferris91@gmail.com>
2014-03-27 21:26:05 -0500

    initialize...

---
+++ MyGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyGame
+//
+//  Created by Jack on 3/27/14.
+//  Copyright (c) 2014 jtferris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
